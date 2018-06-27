0d0aabc96b692bddcf67d05c2c327b1f9d521846
KarinaMealey <kem32002@gmail.com>
2018-06-27 15:34:11 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,5 +1,5 @@
 def get_fortune
-  fortune_array=["you will achieve your dreams today", "you'll have a fun party with some od friends", "you will come across a field of bok choy"]
+  fortune_array=["you will achieve your dreams today", "you'll have a fun party with some od friends", "you will come across a field of bok choy", "you will fall in a hole in 2 days"]
   return  fortune_array.sample 
 end 
 
